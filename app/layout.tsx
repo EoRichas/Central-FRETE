9682df65605bc77bf0e937f041d4cdf26adbc436
EoRichas <Richard.fsoarees@gmail.com>
2026-09-17 09:22:05 -0300

    style: load clean table action refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./dark-mode-action-refinements.css";
 import "./contrast-refinements.css";
 import "./form-refinements.css";
 import "./animation-restore.css";
+import "./action-refinements.css";
 
 export const metadata: Metadata = {
   title: {
